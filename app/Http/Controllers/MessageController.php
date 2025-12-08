91b4572247a8812bd3b19514a03843bd9aae8511
Woopicus <bart_drongelen@hotmail.com>
2025-12-08 13:27:55 +0100

    Added JsonResponse and changed status code to HTTP return type

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -6,6 +6,8 @@ namespace App\Http\Controllers;
 
 use App\Services\MessageService;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class MessageController extends Controller
 {
@@ -16,50 +18,50 @@ class MessageController extends Controller
         $this->messageService = $messageService;
     }
 
-    public function getMessages()
+    public function getMessages(): JsonResponse
     {
         $messages = $this->messageService->getMessages();
 
-        return response()->json(['data' => $messages], 200);
+        return response()->json(['data' => $messages], Response::HTTP_OK);
     }
 
-    public function getMessage(int $messageId)
+    public function getMessage(int $messageId): JsonResponse
     {
         $message = $this->messageService->getMessage($messageId);
 
         if ($message) {
-            return response()->json(['data' => $message], 200);
+            return response()->json(['data' => $message], Response::HTTP_OK);
         }
 
-        return response()->json(['error' => 'Message not found'], 404);
+        return response()->json(['error' => 'Message not found'], Response::HTTP_NOT_FOUND);
     }
 
-    public function createMessage(Request $request)
+    public function createMessage(Request $request): JsonResponse
     {
         $message = $this->messageService->createMessage($request->all());
 
-        return response()->json(['data' => $message], 201);
+        return response()->json(['data' => $message], Response::HTTP_CREATED);
     }
 
-    public function updateMessage(Request $request, int $messageId)
+    public function updateMessage(Request $request, int $messageId): JsonResponse
     {
         $message = $this->messageService->updateMessage($messageId, $request->all());
 
         if ($message) {
-            return response()->json(['data' => $message], 200);
+            return response()->json(['data' => $message], Response::HTTP_OK);
         }
 
-        return response()->json(['error' => 'Message not found'], 404);
+        return response()->json(['error' => 'Message not found'], Response::HTTP_NOT_FOUND);
     }
 
-    public function removeMessage(int $messageId)
+    public function removeMessage(int $messageId): JsonResponse
     {
         $deleted = $this->messageService->removeMessage($messageId);
 
         if ($deleted) {
-            return response()->json([], 204);
+            return response()->json([], Response::HTTP_NO_CONTENT);
         }
 
-        return response()->json(['error' => 'Message not found'], 404);
+        return response()->json(['error' => 'Message not found'], Response::HTTP_NOT_FOUND);
     }
 }
